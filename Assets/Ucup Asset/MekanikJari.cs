7d2b92673623509b8b35b31d6cc37520f5d9a6bb
Raka Ahmad Abrisam <rakaahmadabrisam@gmail.com>
2026-07-01 23:24:28 +0700

    Test 1

--- Assets/Ucup Asset/MekanikJari.cs
+++ Assets/Ucup Asset/MekanikJari.cs
@@ -8,6 +8,12 @@ public class MekanikJari : MonoBehaviour
     public string namaBagianTangan = "Jari Tengah";
     public bool balikArahGerak = false;
 
+    [Header("Pengaturan Kekuatan (Otot)")]
+    [Tooltip("Semakin besar, semakin keras jari menolak ditekuk oleh meja")]
+    public float kekuatanOtot = 5000f; // Default diubah jadi 5000!
+    [Tooltip("Mencegah jari bergetar/memantul")]
+    public float remOtot = 100f;
+
     private Vector2 posisiMouseLama;
     private HingeJoint engsel;
     private JointSpring perEngsel;
@@ -21,8 +27,9 @@ public class MekanikJari : MonoBehaviour
         engsel.useSpring = true;
         perEngsel = engsel.spring;
 
-        perEngsel.spring = 1000f;
-        perEngsel.damper = 50f;
+        // SEKARANG MENGGUNAKAN VARIABEL AGAR BISA DIUBAH DI INSPECTOR
+        perEngsel.spring = kekuatanOtot;
+        perEngsel.damper = remOtot;
         engsel.spring = perEngsel;
 
         kameraUtama = Camera.main;
@@ -61,12 +68,23 @@ public class MekanikJari : MonoBehaviour
         sedangDitarik = true;
         posisiMouseLama = Input.mousePosition;
 
-        Vector3 sumbuDunia = transform.TransformDirection(engsel.axis);
-        Vector3 posisiLayar = kameraUtama.WorldToScreenPoint(transform.position);
-        Vector3 ujungSumbuLayar = kameraUtama.WorldToScreenPoint(transform.position + sumbuDunia);
+        // Simulasi Arah Anti-Terbalik
+        Collider col = GetComponent<Collider>();
+        Vector3 pusatMassaDunia = col != null ? col.bounds.center : transform.position + transform.forward;
+        Vector3 pusatLokal = transform.InverseTransformPoint(pusatMassaDunia);
+        if (pusatLokal.magnitude < 0.01f) pusatLokal = Vector3.forward;
 
-        Vector2 arahSumbuLayar = new Vector2(ujungSumbuLayar.x - posisiLayar.x, ujungSumbuLayar.y - posisiLayar.y).normalized;
-        arahTarikanTerkunci = new Vector2(-arahSumbuLayar.y, arahSumbuLayar.x);
+        Quaternion rotasiSimulasi = Quaternion.AngleAxis(5f, engsel.axis);
+        Vector3 pusatBaruLokal = rotasiSimulasi * pusatLokal;
+
+        Vector3 posisiDuniaAwal = transform.TransformPoint(pusatLokal);
+        Vector3 posisiDuniaBaru = transform.TransformPoint(pusatBaruLokal);
+
+        Vector2 posisiLayarAwal = kameraUtama.WorldToScreenPoint(posisiDuniaAwal);
+        Vector2 posisiLayarBaru = kameraUtama.WorldToScreenPoint(posisiDuniaBaru);
+
+        arahTarikanTerkunci = (posisiLayarBaru - posisiLayarAwal).normalized;
+        if (arahTarikanTerkunci == Vector2.zero) arahTarikanTerkunci = Vector2.up;
 
         if (UIManagerTangan.instance != null)
             UIManagerTangan.instance.SedangDitarik(namaBagianTangan);
@@ -88,7 +106,6 @@ public class MekanikJari : MonoBehaviour
 
         perEngsel.targetPosition += putaran;
 
-        // Nah, bagian ini yang akan membaca settingan "Limits" dari Hinge Joint-mu secara otomatis!
         if (engsel.useLimits)
         {
             perEngsel.targetPosition = Mathf.Clamp(perEngsel.targetPosition, engsel.limits.min, engsel.limits.max);
@@ -108,109 +125,3 @@ public class MekanikJari : MonoBehaviour
         }
     }
 }
-
-
-//using UnityEngine;
-
-//[RequireComponent(typeof(HingeJoint))]
-//public class MekanikJari : MonoBehaviour
-//{
-//    [Header("Pengaturan Jari")]
-//    public float kecepatanPutar = 1f;
-//    public string namaBagianTangan = "Jari Tengah";
-
-//    [Header("Perbaikan Arah")]
-//    [Tooltip("Centang kotak ini jika saat mouse ditarik ke atas, jari malah ke bawah")]
-//    public bool balikArahGerak = false;
-
-//    private Vector2 posisiMouseLama;
-//    private HingeJoint engsel;
-//    private JointSpring perEngsel;
-//    private bool sedangDitarik = false;
-//    private Camera kameraUtama;
-
-//    private Vector2 arahTarikanTerkunci;
-
-//    void Start()
-//    {
-//        engsel = GetComponent<HingeJoint>();
-//        engsel.useSpring = true;
-//        perEngsel = engsel.spring;
-
-//        perEngsel.spring = 1000f;
-//        perEngsel.damper = 50f;
-//        engsel.spring = perEngsel;
-
-//        kameraUtama = Camera.main;
-//    }
-
-//    void OnMouseEnter()
-//    {
-//        if (!sedangDitarik && UIManagerTangan.instance != null)
-//        {
-//            UIManagerTangan.instance.MulaiSorot(namaBagianTangan, transform);
-//        }
-//    }
-
-//    void OnMouseExit()
-//    {
-//        if (!sedangDitarik && UIManagerTangan.instance != null)
-//            UIManagerTangan.instance.BerhentiSorot();
-//    }
-
-//    void OnMouseDown()
-//    {
-//        sedangDitarik = true;
-//        posisiMouseLama = Input.mousePosition;
-
-//        Vector3 sumbuDunia = transform.TransformDirection(engsel.axis);
-//        Vector3 posisiLayar = kameraUtama.WorldToScreenPoint(transform.position);
-//        Vector3 ujungSumbuLayar = kameraUtama.WorldToScreenPoint(transform.position + sumbuDunia);
-
-//        Vector2 arahSumbuLayar = new Vector2(ujungSumbuLayar.x - posisiLayar.x, ujungSumbuLayar.y - posisiLayar.y).normalized;
-//        arahTarikanTerkunci = new Vector2(-arahSumbuLayar.y, arahSumbuLayar.x);
-
-//        if (UIManagerTangan.instance != null)
-//            UIManagerTangan.instance.SedangDitarik(namaBagianTangan);
-//    }
-
-//    void OnMouseUp()
-//    {
-//        LepasPegangan();
-//    }
-
-//    void OnMouseDrag()
-//    {
-//        if (!sedangDitarik) return;
-
-//        Vector2 bedaGeser = (Vector2)Input.mousePosition - posisiMouseLama;
-
-//        // Rumus putaran bawaanmu
-//        float putaran = Vector2.Dot(bedaGeser, arahTarikanTerkunci) * kecepatanPutar;
-
-//        if (balikArahGerak)
-//        {
-//            putaran =- putaran;
-//        }
-
-//        perEngsel.targetPosition += putaran;
-
-//        if (engsel.useLimits)
-//        {
-//            perEngsel.targetPosition = Mathf.Clamp(perEngsel.targetPosition, engsel.limits.min, engsel.limits.max);
-//        }
-
-//        engsel.spring = perEngsel;
-//        posisiMouseLama = Input.mousePosition;
-//    }
-
-//    private void LepasPegangan()
-//    {
-//        if (sedangDitarik)
-//        {
-//            sedangDitarik = false;
-//            if (UIManagerTangan.instance != null)
-//                UIManagerTangan.instance.BerhentiSorot();
-//        }
-//    }
-//}
